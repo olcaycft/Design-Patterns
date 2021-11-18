44ef29e1fd4a47882adb3aadd8966dea6b3ae4c4
Olcay ÇİFTÇİ <olcay_farmer@hotmail.com>
2021-11-19 01:53:18 +0300

    Dependency Injection Design Pattern

---
+++ DependencyInjection/ProductManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    class ProductManager
+    {
+        private IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public void Save()
+        {
+            _productDal.Save();
+        }
+    }
+}
---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new NhbProductDal());
+            productManager.Save();
+        }
+    }
+}
