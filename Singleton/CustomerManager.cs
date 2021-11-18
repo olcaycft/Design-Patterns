3fd6c4e5f887c2c682c27d3165122364e89b2510
Olcay ÇİFTÇİ <olcay_farmer@hotmail.com>
2021-11-19 00:02:49 +0300

    Design Pattern Singleton workshop

---
+++ Singleton/CustomerManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class CustomerManager
+    {
+        private static CustomerManager _customerManager;
+
+        private int _degisken;
+        private CustomerManager()
+        {
+
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved");
+        }
+        public static CustomerManager CreateAsSingleton()
+        {
+            if(_customerManager==null)
+            {
+                _customerManager = new CustomerManager();
+            }
+
+
+            return _customerManager;
+        }
+    }
+}
